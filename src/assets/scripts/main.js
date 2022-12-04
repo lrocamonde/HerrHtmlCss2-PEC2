a7b22b0e1559d425c208aa98c5c9014f0ff35ca7
lrocamonde <lrocamonde@uoc.edu>
2022-12-04 16:38:35 +0100

    Ultima pagina completa

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -16,3 +16,41 @@ import * as bootstrap from 'bootstrap';
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+      alertPlaceholder.innerHTML = "";
+      const wrapper = document.createElement('div')
+      var mensaje = '';
+      var estilo = '';
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+        mensaje = 'Error en el mensaje'
+        estilo = 'danger'
+      } else{
+        mensaje = 'Formulario correcto'
+        estilo = 'success'
+      }
+
+      wrapper.innerHTML = [
+        `<div class="alert alert-${estilo} alert-dismissible" role="alert">`,
+        `   <div>${mensaje}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+      ].join('')
+    
+      alertPlaceholder.append(wrapper)
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
